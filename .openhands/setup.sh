1ec4d5faad386644f7a6da23b0e476e8d50c5449
Jan Jaap <mail@janjaap.de>
2025-08-01 14:42:43 +0200

    fix: git signing key path

--- .openhands/setup.sh
+++ .openhands/setup.sh
@@ -16,12 +16,12 @@ chmod 700 ~/.ssh
 
 # Write the signing key to a file
 if [ -n "$GIT_SIGNING_KEY" ]; then
-    echo "$GIT_SIGNING_KEY" > /home/openhands/.ssh/git_signing_key
-    chmod 600 /home/openhands/.ssh/git_signing_key
+    echo "$GIT_SIGNING_KEY" > "$HOME/.ssh/git_signing_key"
+    chmod 600 "$HOME/.ssh/git_signing_key"
     
     # Configure Git to use the SSH key for signing
     git config --global gpg.format ssh
-    git config --global user.signingkey /home/openhands/.ssh/git_signing_key
+    git config --global user.signingkey "$HOME/.ssh/git_signing_key"
     git config --global commit.gpgsign true
     git config --global tag.gpgsign true
     
