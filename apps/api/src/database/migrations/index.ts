d26d3f9bf2e69947179c2ebdb4ec4181712e8728
Jan Jaap <mail@janjaap.de>
2025-08-16 19:49:13 +0200

    fix: migration breaking system

--- apps/api/src/database/migrations/index.ts
+++ apps/api/src/database/migrations/index.ts
@@ -39,3 +39,4 @@ export * from './1754307072519-nfc-card-last-seen';
 export * from './1755263227552-username-last-changed';
 export * from './1755349210809-password-changed-email';
 export * from './1755364822388-clean-db';
+export * from './1755366278315-clean-db-2';
