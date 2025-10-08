049ce797f4fa6dca999d602e766347b89129bc9a
Jan Jaap <mail@janjaap.de>
2025-10-08 17:33:31 +0200

    fix(ATT-113): allow billing factor to be 0

--- apps/api/src/users-and-auth/users/dtos/changeBillingFactor.dto.ts
+++ apps/api/src/users-and-auth/users/dtos/changeBillingFactor.dto.ts
@@ -1,10 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNumber, IsPositive, Min } from 'class-validator';
+import { IsNumber, Min } from 'class-validator';
 
 export class ChangeBillingFactorDto {
   @ApiProperty({ description: 'The new billing factor', example: 50 })
   @IsNumber()
-  @IsPositive()
   @Min(0)
   billingFactor!: number;
 }
