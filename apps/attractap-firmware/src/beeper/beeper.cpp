20cba0a2b1e15e0a6d543c6b25a438dcabdcb258
Jan Jaap <mail@janjaap.de>
2026-03-28 17:30:07 +0100

    chore: cleanup

--- apps/attractap-firmware/src/beeper/beeper.cpp
+++ apps/attractap-firmware/src/beeper/beeper.cpp
@@ -62,7 +62,7 @@ void Beeper::singleBeep()
     digitalWrite(BEEPER_PIN, HIGH);
 #endif
 
-    delay(200); // 200ms: passive buzzer on V4 hardware needs longer pulse to be audible (V3 used 100ms)
+    delay(200);
 
 #ifdef HAS_IO_EXPANDER
     if (this->ioExpander)
