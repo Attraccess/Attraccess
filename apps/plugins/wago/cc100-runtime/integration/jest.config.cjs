eea3f65a07d2d081b96f1449bfd7155d3af35ff7
Jan Jaap <mail@janjaap.de>
2026-09-08 10:30:55 +0200

    fix(ATT-1048): isolate simulator integration module map

--- apps/plugins/wago/cc100-runtime/integration/jest.config.cjs
+++ apps/plugins/wago/cc100-runtime/integration/jest.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   displayName: 'wago-simulator-broker',
   rootDir: '../../../../..',
   testEnvironment: 'node',
-  modulePathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/apps/plugins/[^/]+/package/'],
+  modulePathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/.nx/cache/', '<rootDir>/apps/plugins/[^/]+/package/'],
   moduleDirectories: ['node_modules', '<rootDir>/node_modules'],
   testMatch: ['<rootDir>/apps/plugins/wago/cc100-runtime/integration/*.integration.ts'],
   transform: {
